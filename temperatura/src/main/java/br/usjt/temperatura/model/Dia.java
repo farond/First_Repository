c805318ecfa24746fd19511faf7f79b9e723b61b
farond <43458363+farond@users.noreply.github.com>
2019-10-16 13:24:35 -0300

    Aula 5

--- temperatura/src/main/java/br/usjt/temperatura/model/Dia.java
+++ temperatura/src/main/java/br/usjt/temperatura/model/Dia.java
@@ -5,6 +5,7 @@ import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -22,7 +23,7 @@ import lombok.ToString;
 public class Dia {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(nullable=false, length=200)
---
+++ temperatura/src/test/java/br/usjt/temperatura/TesteCriarCidade.java
@@ -0,0 +1,5 @@
+package br.usjt.temperatura;
+
+public class TesteCriarCidade {
+
+}
